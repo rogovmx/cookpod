de6e95f5b45e75961f34e71638288e392d1354a3
Mikhail <rogovmx@gmail.com>
2020-05-03 18:30:05 +0300

    integration tests

---
+++ test/integration/registration_test.exs
@@ -0,0 +1,38 @@
+defmodule CookpodWeb.RegistrationTest do
+  use CookpodWeb.IntegrationCase, async: true
+
+  @moduletag basic_auth: true
+
+  test "register with valid params", %{conn: conn} do
+    get(conn, Routes.user_path(conn, :new))
+    |> follow_form(%{
+      user: %{
+        email: "test@test.tst",
+        password: "test1",
+        password_confirmation: "test1"
+      }
+    })
+    |> assert_response(
+      status: 200,
+      path: Routes.page_path(conn, :index),
+      html: "You are logged in as test@test.tst"
+    )
+  end
+
+  test "register with invalid params", %{conn: conn} do
+    get(conn, Routes.user_path(conn, :new))
+    |> follow_form(%{
+      user: %{
+        email: "test",
+        password: "test1",
+        password_confirmation: "test1"
+      }
+    })
+    |> assert_response(
+      status: 422,
+      path: Routes.user_path(conn, :create),
+      html: "Registration"
+    )
+  end
+
+end
